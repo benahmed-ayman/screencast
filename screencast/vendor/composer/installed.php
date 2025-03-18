63e532f3a524bd5c0b5b2edd76477d4916fa1b56
benahmed-ayman <ayman.benahmed@student.hepl.be>
2025-03-18 06:30:24 +0100

    change two

---
+++ screencast/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tecg-dcs-2024-2025/screencast',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2f6173506bc6bc0a83c3d661ca9f53bde20a0c10',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tecg-dcs-2024-2025/screencast' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2f6173506bc6bc0a83c3d661ca9f53bde20a0c10',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
